9b10fac07c6c238ddcd1d00c917537e76dfd8b46
Jess Hendricks <jesshmusic@gmail.com>
2019-08-12 15:08:58 -0400

    App: Reorganized the Models folder

--- config/application.rb
+++ config/application.rb
@@ -13,9 +13,6 @@ module DmCampaignManager
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.autoload_paths += Dir[Rails.root.join('app', 'models', '{*/}')]
   end
 end
