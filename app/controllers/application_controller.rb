d0431d06124abff193281eccac2bb85b24a6fce7
Jess Hendricks <jesshmusic@gmail.com>
2019-08-15 20:09:19 -0400

    Fixed a few linting/code/style errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   include Pundit
   include Pagy::Backend
   protect_from_forgery
+
+  # @return [User]
+  def current_user
+    super
+  end
 end
