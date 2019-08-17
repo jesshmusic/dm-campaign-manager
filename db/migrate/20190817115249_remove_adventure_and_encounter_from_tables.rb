dbdc995f6086fb9f574b449ba5bfa9bf6262833c
Jess Hendricks <jesshmusic@gmail.com>
2019-08-17 16:16:45 -0400

    Campaigns: Adjusted the migration

--- db/migrate/20190817115249_remove_adventure_and_encounter_from_tables.rb
+++ db/migrate/20190817115249_remove_adventure_and_encounter_from_tables.rb
@@ -1,8 +1,8 @@
 class RemoveAdventureAndEncounterFromTables < ActiveRecord::Migration[5.2]
   def change
-    remove_reference :world_locations, :adventure, foreign_key: true
-    remove_reference :world_locations, :encounter, foreign_key: true
-    remove_reference :world_events, :adventure, foreign_key: true
-    remove_reference :world_events, :encounter, foreign_key: true
+    remove_reference :world_locations, :adventure
+    remove_reference :world_locations, :encounter
+    remove_reference :world_events, :adventure
+    remove_reference :world_events, :encounter
   end
 end
