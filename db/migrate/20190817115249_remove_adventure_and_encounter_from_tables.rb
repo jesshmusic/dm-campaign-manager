5bc5ed93d25151da9ec80ddd32b62685ad69a050
Jess Hendricks <jesshmusic@gmail.com>
2019-08-17 16:21:04 -0400

    Attempting to delete a migration

--- db/migrate/20190817115249_remove_adventure_and_encounter_from_tables.rb
+++
@@ -1,8 +0,0 @@
-class RemoveAdventureAndEncounterFromTables < ActiveRecord::Migration[5.2]
-  def change
-    remove_reference :world_locations, :adventure
-    remove_reference :world_locations, :encounter
-    remove_reference :world_events, :adventure
-    remove_reference :world_events, :encounter
-  end
-end
