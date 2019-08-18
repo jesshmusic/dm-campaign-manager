0dd4ed9ac13d88c0ad6f8c0175b31a824ea892f7
Jess Hendricks <jesshmusic@gmail.com>
2019-08-18 16:16:08 -0400

    Home: reorganized the props a bit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,9 @@ class HomeController < ApplicationController
   def index
     puts flash.to_json
     authorize :home, :index?
-    if current_user&.dungeon_master?
+    if current_user&.admin?
+      set_admin_props
+    elsif current_user&.dungeon_master?
       set_dm_props
     elsif current_user
       set_player_props
@@ -15,7 +17,32 @@ class HomeController < ApplicationController
     end
   end
 
+
+
   private
+
+  def set_admin_props
+    @home_props = {
+      user: current_user,
+      campaigns: {
+        title: 'My Campaigns',
+        campaigns: Campaign.where(user: current_user)
+      },
+      nonPlayerCharacters: {
+        title: 'My Non-player Characters',
+        characters: NonPlayerCharacter.where(user: current_user)
+      },
+      playerCharacters: {
+        title: 'My Player Characters',
+        characters: PlayerCharacter.where(user: current_user)
+      },
+      dungeonMasters: {
+        title: 'My Dungeon Masters',
+        dungeonMasters: current_user.campaigns.map(&:dungeon_master)
+      },
+      flashMessages: flash_messages
+    }
+  end
 
   def set_dm_props
     @home_props = {
@@ -24,9 +51,13 @@ class HomeController < ApplicationController
         title: 'My Campaigns',
         campaigns: current_user.campaigns
       },
-      characters: {
-        title: 'My Characters',
-        characters: Character.where(user_id: current_user.id)
+      nonPlayerCharacters: {
+        title: 'My Non-player Characters',
+        characters: NonPlayerCharacter.where(user: current_user)
+      },
+      playerCharacters: {
+        title: 'My Player Characters',
+        characters: PlayerCharacter.where(user: current_user)
       },
       dungeonMasters: {
         title: 'My Dungeon Masters',
@@ -43,9 +74,10 @@ class HomeController < ApplicationController
         title: 'My Campaigns',
         campaigns: current_user.player_campaigns
       },
-      characters: {
-        title: 'My Characters',
-        characters: Character.where(user_id: current_user.id)
+      nonPlayerCharacters: nil,
+      playerCharacters: {
+        title: 'My Player Characters',
+        characters: PlayerCharacter.where(user: current_user)
       },
       dungeonMasters: {
         title: 'My Dungeon Masters',
@@ -62,10 +94,7 @@ class HomeController < ApplicationController
         title: 'Campaigns',
         campaigns: Campaign.all.limit(5)
       },
-      characters: {
-        title: 'Characters',
-        characters: Character.all.limit(5)
-      },
+      nonPlayerCharacters: nil,
       dungeonMasters: {
         title: 'Dungeon Masters',
         dungeonMasters: User.where(role: :dungeon_master).limit(5)
--- app/javascript/bundles/DungeonMasterCampaignManager/components/layout/Footer/Footer.jsx
+++ app/javascript/bundles/DungeonMasterCampaignManager/components/layout/Footer/Footer.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import axiosClient from '../../../actions/axiosClient';
 
 import styles from './footer.module.scss';
 
