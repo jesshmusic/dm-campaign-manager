8071f12ecd9a314d97f23df0b94a3232b8e1226c
Jess Hendricks <jesshmusic@gmail.com>
2019-08-24 15:47:15 -0400

    Moved the banner image to the public folder for now

--- app/javascript/bundles/DungeonMasterCampaignManager/components/layout/HeroBanner/HeroBanner.jsx
+++ app/javascript/bundles/DungeonMasterCampaignManager/components/layout/HeroBanner/HeroBanner.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Image from 'react-bootstrap/Image';
 
 import classes from './hero-banner.module.scss';
 
 const HeroBanner = (props) => {
   const {imagePath} = props;
   return (
-    <Image src={'/assets/rays-of-sun-through-trees.jpg'}/>
+    <div className={classes.heroBanner} style={{backgroundImage: imagePath ? `url(${imagePath})` : 'url(\'/images/rays-of-sun-through-trees.jpg\')'}}/>
   );
 };
 
