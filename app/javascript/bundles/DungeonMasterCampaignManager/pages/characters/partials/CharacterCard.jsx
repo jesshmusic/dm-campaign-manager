b8ba1aa78885e7515f028991d239d7dfdcd90f60
Jess Hendricks <jesshmusic@gmail.com>
2019-09-04 18:07:59 -0400

    Front Page: added more info to the character cards

--- app/javascript/bundles/DungeonMasterCampaignManager/pages/characters/partials/CharacterCard.jsx
+++ app/javascript/bundles/DungeonMasterCampaignManager/pages/characters/partials/CharacterCard.jsx
@@ -6,14 +6,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Card from 'react-bootstrap/Card';
 import { Link } from '@reach/router';
-import ReactMarkdown from 'react-markdown';
 
 const CharacterCard = ({character}) => (
   <Card className={'mb-4'}>
     <Card.Body>
-      <Card.Title>{character.name} {character.type === 'NonPlayerCharacter' ? <small>{character.role}</small> : ''} </Card.Title>
-      <Card.Subtitle className="mb-2 text-muted">{character.classes}</Card.Subtitle>
+      <Card.Title>{character.name} {character.type === 'NonPlayerCharacter' ? <small className="text-muted">&ldquo;{character.role}&rdquo;</small> : ''} </Card.Title>
+      <Card.Subtitle className="text-muted">{character.classes}</Card.Subtitle>
       <h6><strong>Campaigns: </strong> {character.campaignsString}</h6>
+      <div className={'d-flex'}>
+        <span className={'mr-4'}><strong>Alignment: </strong>{character.alignment}</span>
+        <span className={'mr-4'}><strong>Armor Class: </strong>{character.armorClass}</span>
+        <span><strong>Hit Points: </strong>{character.hitPoints}</span>
+      </div>
     </Card.Body>
     <Card.Footer>
       <Link to={`/app/characters/${character.slug}`} className='btn btn-primary'>Details</Link>
