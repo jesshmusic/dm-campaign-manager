45bb508cdf8ed3f06ca80c2292cbe9ddf09ce91f
Jess Hendricks <jesshmusic@gmail.com>
2019-10-13 11:17:04 -0400

    Forms: switched to the CKEditor because TinyMCE was shite.

--- app/javascript/bundles/DungeonMasterCampaignManager/components/forms/FormRichTextArea.jsx
+++ app/javascript/bundles/DungeonMasterCampaignManager/components/forms/FormRichTextArea.jsx
@@ -7,11 +7,7 @@ import PropTypes from 'prop-types';
 import Col from 'react-bootstrap/Col';
 import {Field} from 'react-final-form';
 import Form from 'react-bootstrap/Form';
-import { Editor } from '@tinymce/tinymce-react';
-
-const handleEditorChange = (content, input) => {
-  return input.onChange(content);
-};
+import CKEditor from '@ckeditor/ckeditor5-react';
 
 const FormRichTextArea = ({name, label, colWidth}) => (
   <Form.Group as={Col} md={colWidth} controlId="validationCustom02">
@@ -19,23 +15,39 @@ const FormRichTextArea = ({name, label, colWidth}) => (
       {({ input, meta }) => (
         <div>
           <Form.Label>{label}</Form.Label>
-          <Editor
-                apiKey={'p7j8xfgttb661xyhyd0c2p0kn9hma0ivisrd48vn7zjzl0vh'}
-                initialValue={input.value}
-                init={{
-                  height: 500,
-                  menubar: false,
-                  plugins: [
-                    'advlist autolink lists link image charmap print preview anchor',
-                    'searchreplace visualblocks code fullscreen',
-                    'insertdatetime media table paste code help wordcount',
+          <CKEditor
+              editor={ ClassicEditor }
+              data={input.value}
+              config={{
+                toolbar: [
+                  'heading',
+                  '|',
+                  'bold',
+                  'italic',
+                  'link',
+                  '|',
+                  'bulletedList',
+                  'numberedList',
+                  'blockQuote',
+                  '|',
+                  'insertTable',
+                  'undo',
+                  'redo',
+                ],
+                heading: {
+                  options: [
+                    { model: 'paragraph', title: 'Paragraph', class: '' },
+                    { model: 'heading2', view: 'h2', title: 'Heading 2', class: '' },
+                    { model: 'heading3', view: 'h3', title: 'Heading 3', class: '' },
+                    { model: 'heading4', view: 'h4', title: 'Heading 4', class: '' },
+                    { model: 'heading5', view: 'h5', title: 'Heading 5', class: '' },
+                    { model: 'heading6', view: 'h6', title: 'Heading 6', class: '' },
                   ],
-                  toolbar:
-                    'undo redo | formatselect | bold italic forecolor backcolor | \
-                    alignleft aligncenter alignright alignjustify | \
-                    bullist numlist outdent indent | removeformat | help',
-                }}
-                onEditorChange={(content) => input.onChange(content)}
+                },
+              }}
+              onChange={ ( event, editor ) => {
+                input.onChange(editor.getData());
+              } }
           />
           <Form.Control.Feedback type="invalid">{meta.error}</Form.Control.Feedback>
         </div>
