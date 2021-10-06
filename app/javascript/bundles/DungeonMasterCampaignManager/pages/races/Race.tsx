09a1e95e2bd453606da7f269d0c4cbf203eb8f5b
jesshmusic <jhendricks@existentialmusic.com>
2021-10-06 09:55:48 -0400

    feat: adds a little styling to the Race page

--- app/javascript/bundles/DungeonMasterCampaignManager/pages/races/Race.tsx
+++ app/javascript/bundles/DungeonMasterCampaignManager/pages/races/Race.tsx
@@ -6,10 +6,12 @@ import PageContainer from '../../containers/PageContainer';
 import PageTitle from '../../components/layout/PageTitle';
 import DndSpinner from '../../components/layout/DndSpinner';
 
+const styles = require('./races.module.scss');
+
 const Race = (props: {
-  race?: RaceProps,
-  raceSlug: string,
-  getRace: (raceSlug: string) => void
+  race?: RaceProps;
+  raceSlug: string;
+  getRace: (raceSlug: string) => void;
 }) => {
   const { race, raceSlug, getRace } = props;
 
@@ -21,26 +23,34 @@ const Race = (props: {
 
   const abilityMods = () => {
     if (race) {
+      if (race.name === 'Human') {
+        return 'Your ability scores each increase by 1.';
+      }
       const mods: string[] = [];
       if (race.strengthModifier !== 0) {
-        mods.push(`Strength: ${race.strengthModifier}`);
+        mods.push(`your strength increases by ${race.strengthModifier}`);
       }
       if (race.dexterityModifier !== 0) {
-        mods.push(`Dexterity: ${race.dexterityModifier}`);
+        mods.push(`your dexterity increases by ${race.dexterityModifier}`);
       }
       if (race.constitutionModifier !== 0) {
-        mods.push(`Constitution: ${race.constitutionModifier}`);
+        mods.push(
+          `your constitution increases by ${race.constitutionModifier}`
+        );
       }
       if (race.intelligenceModifier !== 0) {
-        mods.push(`Intelligence: ${race.intelligenceModifier}`);
+        mods.push(
+          `your intelligence increases by ${race.intelligenceModifier}`
+        );
       }
       if (race.wisdomModifier !== 0) {
-        mods.push(`Wisdom: ${race.wisdomModifier}`);
+        mods.push(`your wisdom increases by ${race.wisdomModifier}`);
       }
       if (race.charismaModifier !== 0) {
-        mods.push(`Charisma: ${race.charismaModifier}`);
+        mods.push(`your charisma increases by ${race.charismaModifier}`);
       }
-      return mods.join(', ');
+      const result = mods.join(', and ') + '.';
+      return result.charAt(0).toUpperCase() + result.slice(1);
     }
     return '';
   };
@@ -49,28 +59,33 @@ const Race = (props: {
     <PageContainer
       breadcrumbs={[
         { url: '/app/races', isActive: false, title: 'Races' },
-        { isActive: true, title: raceTitle }
+        { isActive: true, title: raceTitle },
       ]}
       description={`Monster: ${raceTitle}. Dungeon Master's Toolbox is a free resource for DMs to manage their dndClasses, adventures, and Monsters.`}
-      pageTitle={raceTitle}>
-      <PageTitle title={raceTitle} />
+      pageTitle={raceTitle}
+    >
       {race ? (
-        <div className='container'>
+        <div className="container">
+          <PageTitle title={raceTitle} />
           <div>
             <div>
-              <strong>Ability Bonuses:</strong> {abilityMods()}
+              <span className={styles.traitName}>Ability Score Increase. </span>
+              {abilityMods()}
             </div>
             <div>
-              <strong>Speed:</strong> {race.speed} ft.
+              <span className={styles.traitName}>Speed. </span> {race.speed} ft.
             </div>
             <div>
-              <strong>{race.size}.</strong> {race.sizeDescription}
+              <span className={styles.traitName}>Size. </span>
+              {race.sizeDescription}
             </div>
             <div>
-              <strong>Age.</strong> {race.age}
+              <span className={styles.traitName}>Age. </span>
+              {race.age}
             </div>
             <div>
-              <strong>Languages.</strong> {race.languageDescription}
+              <span className={styles.traitName}>Languages. </span>{' '}
+              {race.languageDescription}
               <p>Starting languages: {race.languages.join(', ')}</p>
               {race.languageChoices.length > 0 ? (
                 <p>Choose from: {race.languageChoices.join(', ')}</p>
@@ -78,14 +93,16 @@ const Race = (props: {
             </div>
           </div>
         </div>
-      ) : <DndSpinner />}
+      ) : (
+        <DndSpinner />
+      )}
     </PageContainer>
   );
 };
 
 function mapStateToProps(state) {
   return {
-    race: state.races.currentRace
+    race: state.races.currentRace,
   };
 }
 
@@ -93,7 +110,7 @@ function mapDispatchToProps(dispatch) {
   return {
     getRace: (raceSlug: string) => {
       dispatch(rest.actions.getRace({ slug: raceSlug }));
-    }
+    },
   };
 }
 
