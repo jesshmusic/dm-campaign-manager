ce4e3efc0b4d21686b571537e2ef32eb64f6c46c
jesshmusic <jesshmusic72@gmail.com>
2025-12-03 12:45:21 -0500

    Add debug logging for JWT authentication

--- app/controllers/secured_controller.rb
+++ app/controllers/secured_controller.rb
@@ -4,8 +4,12 @@ class SecuredController < ApplicationController
   private
 
   def authorize_request
+    auth_header = request.headers['Authorization']
+    Rails.logger.info "Authorization header present: #{auth_header.present?}"
+    Rails.logger.info "Authorization header: #{auth_header&.truncate(50)}" if auth_header.present?
     AuthorizationService.new(request.headers).authenticate_request!
-  rescue JWT::VerificationError, JWT::DecodeError
+  rescue JWT::VerificationError, JWT::DecodeError => e
+    Rails.logger.error "JWT Error: #{e.class} - #{e.message}"
     render json: { errors: ['Not Authenticated'] }, status: :unauthorized
   end
 end
